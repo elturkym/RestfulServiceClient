e4dcfa118e782ba936ace75939003d5810bb3588
mo7medhosny <mo7medhosny@gmail.com>
2016-08-15 00:12:00 -0500

    Use Link property in the json object

--- ajax.js
+++ ajax.js
@@ -1,97 +1,101 @@
 "use strict";
 
-var urlRoot = 'http://localhost:8080/RestfulService/';
+(function() {
+	
+	var urlRoot = 'http://restfulservice.cfapps.io/';
 
-$(function() {
-	$("#userForm").submit(getUserData);
-});
-
-function getUserData(event) {
-	if ($("#userId").val()) {
-		$.ajax(urlRoot + 'users/' + $("#userId").val(), {type: 'get', dataType: 'json'}).done(processSuccess)
-				.fail(processFail);
-	}
-	event.preventDefault();
-}
-
-function processSuccess(user) {
-	$("#mainDiv").empty();
-	$('<h1>').text(user.name).appendTo($("#mainDiv"));
-	$('<p>').text("Address: " + user.address).appendTo($("#mainDiv"));
-	$('<p>').text("Email: " + user.email).appendTo($("#mainDiv"));
-	getPosts();
-}
-
-function getPosts() {
-	var u = $("#userId").val();
-	$.ajax(urlRoot + 'users/' + $("#userId").val() + '/posts', {type: 'get', dataType: 'json'}).done(
-			processPostsSuccess).fail(processFail);
-}
-
-function processPostsSuccess(posts) {
-	posts.forEach(function(obj) {
-		var div = $('<div id=div-' + obj.id + '>');
-
-		var body = $('<p class=\'post\'>');
-		body.text(obj.text);
-
-		var commentDiv = $('<div id=comments-' + obj.id + '>');
-		commentDiv.hide();
-
-		var commentShowButton = $('<input class=\'comment\' id=comments-' + obj.id + '-b type=\'button\' value=\'Show Comments\'>');
-		commentShowButton.click(displayHideComments(obj.id));
-		
-		div.append(body).append(commentDiv).append(commentShowButton);
-		$("#mainDiv").append(div);
+	$(function() {
+		$("#userForm").submit(getUserData);
 	});
-}
 
-function displayHideComments(postId) {
-	return function() {
-		var div = $('#comments-' + postId);
-		if (div.is(":visible")) {
-			div.empty();
-			div.hide();
-			$('#comments-' + postId + '-b').val('Show Comments');
-		} else {
-			getComments(postId);
+	function getUserData(event) {
+		if ($("#userId").val()) {
+			$.ajax(urlRoot + 'users/' + $("#userId").val(), {type: 'get', dataType: 'json'}).done(processSuccess)
+					.fail(processFail);
 		}
-	};
-}
+		event.preventDefault();
+	}
 
-function getComments(postId) {
-	var u = $("#userId").val();
-	$.ajax(urlRoot + 'posts/' + postId + '/comments', {type: 'get', dataType: 'json'}).done(
-			getProcessCommentsSuccessFun(postId)).fail(processFail);
-}
+	function processSuccess(user) {
+		$("#mainDiv").empty();
+		$('<h1>').text(user.name).appendTo($("#mainDiv"));
+		$('<p>').text("Address: " + user.address).appendTo($("#mainDiv"));
+		$('<p>').text("Email: " + user.email).appendTo($("#mainDiv"));
+		getPosts(user.links[0].href);
+	}
 
-function getProcessCommentsSuccessFun(postId) {
-	var myFun = processCommentsSuccess.bind({}, postId);
-	return myFun;
-}
+	function getPosts(postsLink) {
+		var u = $("#userId").val();
+		$.ajax(postsLink, {type: 'get', dataType: 'json'}).done(
+				processPostsSuccess).fail(processFail);
+	}
 
-function processCommentsSuccess(postId, comments) {
-	var div = $('#comments-' + postId);
-	if (comments)  {
-		comments.forEach(function(obj) {
-	
-			var cDiv = $('<div>');
+	function processPostsSuccess(posts) {
+		posts.forEach(function(post) {
+			var div = $('<div id=div-' + post.id + '>');
+
+			var body = $('<p class=\'post\'>');
+			body.text(post.text);
+
+			var commentDiv = $('<div id=comments-' + post.id + '>');
+			commentDiv.hide();
+
+			var commentShowButton = $('<input class=\'comment\' id=comments-' + post.id + '-b type=\'button\' value=\'Show Comments\'>');
+			commentShowButton.click(displayHideComments(post.links[0].href, post.id));
 			
-			var userHead = $('<em>');
-			userHead.text(obj.user.name);
-
-			var body = $('<p>');
-			body.text(obj.text);
-			
-			cDiv.append(userHead).append(body).append($('<hr>'));
-			div.append(cDiv);
+			div.append(body).append(commentDiv).append(commentShowButton);
+			$("#mainDiv").append(div);
 		});
-	}	
-	
-	div.show();
-	$('#comments-' + postId + '-b').val('Hide Comments');
-}
+	}
 
-function processFail() {
-	alert("Can't fetch the data from server");
-}
+	function displayHideComments(commentsLink, postId) {
+		return function() {
+			var div = $('#comments-' + postId);
+			if (div.is(":visible")) {
+				div.empty();
+				div.hide();
+				$('#comments-' + postId + '-b').val('Show Comments');
+			} else {
+				getComments(commentsLink, postId);
+			}
+		};
+	}
+
+	function getComments(commentsLink, postId) {
+		var u = $("#userId").val();
+		$.ajax(commentsLink, {type: 'get', dataType: 'json'}).done(
+				getProcessCommentsSuccessFun(postId)).fail(processFail);
+	}
+
+	function getProcessCommentsSuccessFun(postId) {
+		var myFun = processCommentsSuccess.bind({}, postId);
+		return myFun;
+	}
+
+	function processCommentsSuccess(postId, comments) {
+		var div = $('#comments-' + postId);
+		if (comments)  {
+			comments.forEach(function(obj) {
+		
+				var cDiv = $('<div>');
+				
+				var userHead = $('<em>');
+				userHead.text(obj.user.name);
+
+				var body = $('<p>');
+				body.text(obj.text);
+				
+				cDiv.append(userHead).append(body).append($('<hr>'));
+				div.append(cDiv);
+			});
+		}	
+		
+		div.show();
+		$('#comments-' + postId + '-b').val('Hide Comments');
+	}
+
+	function processFail() {
+		alert("Can't fetch the data from server");
+	}
+
+}) ();
